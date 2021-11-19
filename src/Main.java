0b6ac95adb98008464a2ee9217d45a6d90ba033c
Kenneth <keennetk@ntnu.no>
2021-11-19 09:15:32 +0100

    Updated tester added scanner

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,43 @@
+
+import java.util.Scanner;  // Import the Scanner class
+
 public class Main {
 
     public static void main(String[] args) {
 
         //Test 1
-        double hours = 8;
-        int packages = 1200;
-        double packageHour = packageHour(hours,packages);
-        System.out.println("Input: [" + hours + "," + packages + "]");
-        System.out.println("Output: " + packageHour);
+        test(3,400);
 
-        //Test 1
-        hours = 7.5;
-        packages = 1250;
-        packageHour = packageHour(hours,packages);
-        System.out.println("Input: [" + hours + "," + packages + "]");
-        System.out.println("Output: " + packageHour);
+        //Test 2
+        test(7.5,1200);
 
+        //Test 2
+        test(5.5,1200);
+
+        // Test 3
+        Scanner myObj = new Scanner(System.in);  // Scanner
+
+        System.out.println("Enter hours worked:");
+        String hoursString = myObj.nextLine();  // Read user input
+        System.out.println("Enter packages:");
+        String packagesString = myObj.nextLine();  // Read user input
+
+        //converting String to double
+        double hours = Double.parseDouble(hoursString);
+        int packages = Integer.parseInt(packagesString);
+
+        test(hours,packages);
+        // Test 3 end
     }
 
     //Function to Calculate packages per hour
     public static double packageHour(double hours, int packages) {
         return packages/hours;
+    }
+
+    //Tester
+    public static void test(double hours, int packages) {
+        System.out.println("Input: [" + hours + "," + packages + "]");
+        System.out.println("Output: " + String.format("%.2f", packageHour(hours,packages)));
     }
 }
